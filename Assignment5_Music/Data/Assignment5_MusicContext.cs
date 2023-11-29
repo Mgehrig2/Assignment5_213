be9be3f6e8869baeabcbfd2758373008d27f6fcd
Taya Morinville <taya.morinville@ndus.edu>
2023-11-29 13:57:03 -0600

    Created Music Model

---
+++ Assignment5_Music/Data/Assignment5_MusicContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment5_Music.Models;
+
+namespace Assignment5_Music.Data
+{
+    public class Assignment5_MusicContext : DbContext
+    {
+        public Assignment5_MusicContext (DbContextOptions<Assignment5_MusicContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment5_Music.Models.Music> Music { get; set; } = default!;
+    }
+}
